ba745473bcf50672a86188047939badb6141c8bb
Mst-Jui <contact.juiakter@gmail.com>
2026-07-02 07:57:54 +0600

    homee

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,52 +1,22 @@
-// import { NextIntlClientProvider } from "next-intl";
-// import { getMessages } from "next-intl/server";
-// import { AppProvider } from "@/context/AppContext";
-// import Navbar from "@/components/Navbar";
-
-// export default async function LocaleLayout({ children, params: { locale } }) {
-//   const messages = await getMessages();
-
-//   return (
-//     <html lang={locale}>
-//       <body>
-//         <NextIntlClientProvider locale={locale} messages={messages}>
-//           <AppProvider>
-//             <Navbar />
-//             <main>{children}</main>
-//           </AppProvider>
-//         </NextIntlClientProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-
-
-
-
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { AppProvider } from "@/context/AppContext";
 import Navbar from "@/components/Navbar";
 
-// বিল্ডের সময় স্ট্যাটিক রাউট জেনারেট করার জন্য এটি যোগ করা হয়েছে
 export async function generateStaticParams() {
   return [{ locale: 'en' }, { locale: 'bn' }];
 }
 
-export default async function LocaleLayout({ children, params: { locale } }) {
+export default async function LocaleLayout({ children, params }) {
+  const { locale } = await params; // Next.js 15+/16 এ params এখন Promise, তাই await লাগবে
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <AppProvider>
-            <Navbar />
-            <main>{children}</main>
-          </AppProvider>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <AppProvider>
+        <Navbar />
+        <main>{children}</main>
+      </AppProvider>
+    </NextIntlClientProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import Banner from "@/components/home/Banner";
+// import Banner from "@/components/home/Banner";
 import CounterSection from "@/components/home/CounterSection";
 
 
-import PackageSection from "../package/page";
+
 import MembershipSection from "@/components/home/MembershipSection";
 import ChairmanSection from "@/components/home/ChairmanSection";
 import DealerFormSection from "@/components/home/DealerFormSection";
@@ -10,6 +10,10 @@ import FAQSection from "@/components/home/FAQSection";
 import ImpactGallerySection from "@/components/home/ImpactGallerySection";
 import WhyChooseUsSection from "@/components/home/WhyChooseUsSection";
 import ProcessStepsSection from "@/components/home/ProcessStepsSection";
+import PackageSection from "@/components/home/PackageSection";
+import Banner from "@/components/home/Banner";
+
+
 
 export default function Home() {
   return (
